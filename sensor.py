5997da19d3516805b1666a7d065822ef7b62d92b
Fajar Nugraha <88820254+fajarnu@users.noreply.github.com>
2022-07-15 15:29:48 +0700

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,15 +1,15 @@
 import time #mengimport modul time
 import board #digunakan untuk menentukan dengan pin GPIO mana kita menghubungkan sensor DHT11.
-import adafruit_dht #library sensor DHT
+import Adafruit_DHT #library sensor DHT
 import psutil #library untuk mengambil informasi tentang proses yang sedang berjalan
 
 
 for proc in psutil.process_iter(): 
     if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
         proc.kill()
-sensor = Adafruit_DHT.DHT11 
+sensor = Adafruit_DHT.DHT11 #mendeklarasikan variabel
 DHT_PIN = 4 #mendefinisikan pin data sensor. Ubah nomor pin jika ingin menggunakan pin lain.
-while True: #pengkondisian 
+while True: #pengondisian
     try:
         humidity, temperature = Adafruit_DHT. read(sensor, DHT_PIN) #membaca nilai kelembaban dan suhu
         print("Temperature: {}*C Humidity: {}".format(temperature, humidity)) #mencetak hasilnya di layar. Mempertimbangkan akurasi sensor yang terbatas, hasilnya diformat tanpa desimal. 
@@ -17,7 +17,7 @@ while True: #pengkondisian
         print(error.args[0]) 
         time.sleep(1.0) #program akan diulang setiap 1 detik
         continue
-    except Exception as error: #dijalankan ketika pengecualian error 
+    except Exception as error: #dijalankan ketika exception error 
         sensor.exit()
         raise error
     time.sleep(1.0) #program akan diulang setiap 1 detik
